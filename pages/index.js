58870332c6cdf0d9aef99ab2f47a7f8ea47979ae
Terran Kartik Tellus <91951646+TerranKartikTellus@users.noreply.github.com>
2022-12-10 14:34:34 +0530

    added loading state to login

--- pages/index.js
+++ pages/index.js
@@ -3,14 +3,14 @@ import Link from "next/link";
 import { useState } from "react";
 
 export default function Home({contacts}) {
-
+const [loading,setloading] = useState("Login");
 
   return (
     <div className="w-full h-screen">
       <div className="w-full h-screen bg flex flex-row items-center justify-start">
         <Link className="w-1/2 hover:bg-green-600 bg-green-500 h-full flex flex-row items-center justify-center text-2xl text-white font-Maven font-bold" href={"/user"} >User <br/> </Link>
-        <Link href="/login" className="w-1/2 text-center city-90 bg-green-700 h-full flex flex-col items-center justify-center text-2xl text-white font-Maven ">
-          Login
+        <Link onClick={()=>{setloading("Loading")}} href="/login" className="w-1/2 text-center city-90 bg-green-700 h-full flex flex-col items-center justify-center text-2xl text-white font-Maven ">
+          {loading}
          
 
           
